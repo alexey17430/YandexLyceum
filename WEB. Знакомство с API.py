6a96d7953e7afa837d782759b3ae62512ff8b6de
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-01-24 11:06:28 +0500

    Получение информации, по ссылке-запросу к яндекс.картам, к какому федеральному округу относится данный город

--- WEB. Знакомство с API.py
+++ WEB. Знакомство с API.py
@@ -1,20 +1,17 @@
 import requests
+from pprint import pprint
 
-geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-" \
-                   "98533de7710b&geocode=Исторический+музей+города+Москва,Красная+площадь,1" \
-                   "&format=json&results=1"
+for elem in ['Хабаровск', 'Уфа', 'Нижний Новгород', 'Калининград']:
+    geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba-" \
+                       f"98533de7710b&geocode={elem}" \
+                       f"&format=json&results=1"
+    response = requests.get(geocoder_request)
 
-response = requests.get(geocoder_request)
-if response:
-    ans1 = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
-        'Point']['pos']
-    ans2 = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
-        'metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['AddressLine']
-    ans3 = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
-        'metaDataProperty']['GeocoderMetaData']['AddressDetails']['Country']['AdministrativeArea'][
-        'Locality']['Thoroughfare']['Premise']['PostalCode']['PostalCodeNumber']
-    print(ans1, ans3 + ' ' + ans2, sep='\n')
-else:
-    print("Ошибка выполнения запроса:")
-    print(geocoder_request)
-    print("Http статус:", response.status_code, "(", response.reason, ")")
+    if response:
+        ans = response.json()['response']['GeoObjectCollection']['featureMember'][0]['GeoObject'][
+            'metaDataProperty']['GeocoderMetaData']['Address']['Components'][1]['name']
+        pprint(ans)
+    else:
+        print("Ошибка выполнения запроса:")
+        print(geocoder_request)
+        print("Http статус:", response.status_code, "(", response.reason, ")")
