41c1e2372abe18225b6b2820798e7f1030d4061f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-01-26 17:18:47 +0500

    Азбука Морзе 2

--- Что такое QT и PyQT. Знакомство.py
+++ Что такое QT и PyQT. Знакомство.py
@@ -10,59 +10,36 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 500, 100)
-        self.setWindowTitle('Арифмометр')
+        self.setGeometry(300, 300, 390, 100)
+        self.setWindowTitle('Азбука Морзе 2')
+        self.morse = {'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
+                      'G': '--.',
+                      'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--',
+                      'N': '-.',
+                      'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+                      'U': '..-',
+                      'V': '...-', 'W': '.--', 'X': '-..-',
+                      'Y': '-.--', 'Z': '--..'}
 
-        self.name_input1 = QLineEdit(self)
-        self.name_input1.move(35 + 0, 35)
-        self.name_input1.resize(100, 30)
-        self.name_input1.setText('0')
+        coef_x = 0
+        coef_y = 0
+        for name in self.morse.keys():
+            btn = QPushButton(self)
+            btn.resize(30, 30)
+            btn.move(coef_x * 30, coef_y * 30)
+            btn.setText(name)
+            btn.clicked.connect(self.pushed_button)
+            coef_x += 1
+            if coef_x == 13:
+                coef_x = 0
+                coef_y = 1
+        self.line = QLineEdit(self)
+        self.line.move(0, 60)
+        self.line.resize(390, 40)
 
-        self.name_input2 = QLineEdit(self)
-        self.name_input2.move(35 + 220, 35)
-        self.name_input2.resize(100, 30)
-        self.name_input2.setText('0')
-
-
-        self.btn1 = QPushButton(self)
-        self.btn1.setText('+')
-        self.btn1.move(35 + 100, 35)
-        self.btn1.resize(40, 30)
-        self.btn1.clicked.connect(self.button_pushed)
-
-        self.btn2 = QPushButton(self)
-        self.btn2.setText('-')
-        self.btn2.move(35 + 140, 35)
-        self.btn2.resize(40, 30)
-        self.btn2.clicked.connect(self.button_pushed)
-
-        self.btn3 = QPushButton(self)
-        self.btn3.setText('*')
-        self.btn3.move(35 + 180, 35)
-        self.btn3.resize(40, 30)
-        self.btn3.clicked.connect(self.button_pushed)
-
-        self.lbl = QLabel(self)
-        self.lbl.setText('=')
-        self.lbl.move(35 + 321, 42)
-
-        self.name_input3 = QLineEdit(self)
-        self.name_input3.move(35 + 330, 35)
-        self.name_input3.resize(100, 30)
-        self.name_input3.setDisabled(True)
-
-    def button_pushed(self):
-        first = str(self.name_input1.text())
-        if len(first) == 0:
-            first = 0
-        else:
-            first = int(first)
-        second = str(self.name_input2.text())
-        if len(second) == 0:
-            second = 0
-        else:
-            second = int(second)
-        self.name_input3.setText(str(eval(f'{first} {self.sender().text()} {second}')))
+    def pushed_button(self):
+        st = self.sender().text()
+        self.line.setText(self.line.text() + self.morse[st])
 
 
 if __name__ == '__main__':
