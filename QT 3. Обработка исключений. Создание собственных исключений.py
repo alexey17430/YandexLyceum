c622ddadbcfa000a77af4a58b8cb2030fd9b025b
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-01-29 16:31:30 +0500

    Выбор пароля

--- QT 3. Обработка исключений. Создание собственных исключений.py
+++ QT 3. Обработка исключений. Создание собственных исключений.py
@@ -1,50 +1,83 @@
-# LBYL
-password = input().strip()
+class PasswordError(Exception):
+    pass
 
 
-def main(st):
-    try:
-        assert len(st) > 8
-        assert st != st.lower()
-        flag = False
-        for i in st:
-            if i in '1234567890':
-                flag = True
-                break
-        assert flag
-        # english PC
-        line1 = 'qwertyuiop'
-        line2 = 'asdfghjkl'
-        line3 = 'zxcvbnm'
-        for i in range(len(st) - 2):
-            elem = st[i: i + 3].lower()
-            assert elem not in line1
-            assert elem not in line2
-            assert elem not in line3
-
-        # english mac
-        line1 = 'qwertzuiop'
-        line2 = 'asdfghjkl'
-        line3 = 'yxcvbnm'
-        for i in range(len(st) - 2):
-            elem = st[i: i + 3].lower()
-            assert elem not in line1
-            assert elem not in line2
-            assert elem not in line3
-        # russian
-        line1 = 'йцукенгшщзхъ'
-        line2 = 'фывапролджэё'
-        line3 = 'ячсмитьбю'
-        for i in range(len(st) - 2):
-            elem = st[i: i + 3].lower()
-            assert elem not in line1
-            assert elem not in line2
-            assert elem not in line3
-        raise IndexError
-    except AssertionError:
-        return 'error'
-    except IndexError:
-        return 'ok'
+class LengthError(PasswordError):
+    pass
 
 
-print(main(password))
+class LetterError(PasswordError):
+    pass
+
+
+class DigitError(PasswordError):
+    pass
+
+
+class SequenceError(PasswordError):
+    pass
+
+
+def check_password():
+    while True:
+        st = input()
+
+        try:
+            if st == 'Ctrl+Break':
+                raise KeyboardInterrupt
+            if len(st) <= 8:
+                raise LengthError
+            if st == st.lower() or st == st.upper():
+                raise LetterError
+            flag = False
+            for i in st:
+                if i in '1234567890':
+                    flag = True
+                    break
+            if not flag:
+                raise DigitError
+            # english PC
+            line1 = 'qwertyuiop'
+            line2 = 'asdfghjkl'
+            line3 = 'zxcvbnm'
+            for i in range(len(st) - 2):
+                elem = st[i: i + 3].lower()
+                if elem in line1:
+                    raise SequenceError
+                elif elem in line2:
+                    raise SequenceError
+                elif elem in line3:
+                    raise SequenceError
+
+            # english mac
+            line1 = 'qwertzuiop'
+            line2 = 'asdfghjkl'
+            line3 = 'yxcvbnm'
+            for i in range(len(st) - 2):
+                elem = st[i: i + 3].lower()
+                if elem in line1:
+                    raise SequenceError
+                elif elem in line2:
+                    raise SequenceError
+                elif elem in line3:
+                    raise SequenceError
+            # russian
+            line1 = 'йцукенгшщзхъ'
+            line2 = 'фывапролджэё'
+            line3 = 'ячсмитьбю'
+            for i in range(len(st) - 2):
+                elem = st[i: i + 3].lower()
+                if elem in line1:
+                    raise SequenceError
+                elif elem in line2:
+                    raise SequenceError
+                elif elem in line3:
+                    raise SequenceError
+            return 'ok'
+        except Exception as error:
+            print(error.__class__.__name__)
+        except KeyboardInterrupt:
+            return 'Bye-Bye'
+
+
+print(check_password())
