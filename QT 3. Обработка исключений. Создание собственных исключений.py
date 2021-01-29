74a079fcfe68fb754c44d40aae46f812b12bd69d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-01-29 16:30:24 +0500

    Телефоны наносят ответный удар

--- QT 3. Обработка исключений. Создание собственных исключений.py
+++ QT 3. Обработка исключений. Создание собственных исключений.py
@@ -1,70 +1,52 @@
-class PasswordError(Exception):
-    pass
+stroka = input().replace(' ', '')
 
 
-class LengthError(PasswordError):
-    pass
+def main(st):
+    try:
+        if st[0] != '8' and st[:2] != '+7':
+            raise ValueError
+        new_st = ''
+        otk = 0
+        clo = 0
+        if st.startswith('-') or st.endswith('-') or \
+                ('(' in st and ')' in st and st.index(')') < st.index('(')):
+            raise ValueError
+
+        for j in range(len(st)):
+            i = st[j]
+            if j + 1 != len(st) and st[j] + st[j + 1] == '--':
+                raise ValueError
+            if i == '(':
+                otk += 1
+            elif i == ')':
+                clo += 1
+            elif i != ' ' and i != '-' and i != '(' and i != ')' and i in '+1234567890':
+                new_st += i
+            elif i not in '+1234567890() -\t':
+                raise ValueError
+        if otk == clo == 1 or otk == clo == 0:
+            pass
+        else:
+            raise ValueError
+        if new_st.startswith('8'):
+            new_st = '+7' + new_st[1:]
+        elem = new_st[2:4]
+        flagok = False
+        for i in ['902', '903', '904', '905', '906']:
+            if new_st[2:5] == i:
+                flagok = True
+                break
+        if len(new_st) != 12:
+            raise IndexError
+        if elem == '91' or elem == '92' or elem == '93' or elem == '98' or elem == '96' or flagok:
+            pass
+        else:
+            return 'не определяется оператор сотовой связи'
+        return new_st
+    except ValueError:
+        return 'неверный формат'
+    except IndexError:
+        return 'неверное количество цифр'
 
 
-class LetterError(PasswordError):
-    pass
-
-
-class DigitError(PasswordError):
-    pass
-
-
-class SequenceError(PasswordError):
-    pass
-
-
-def check_password(st):
-    if len(st) <= 8:
-        raise LengthError
-    if st == st.lower() or st == st.upper():
-        raise LetterError
-    flag = False
-    for i in st:
-        if i in '1234567890':
-            flag = True
-            break
-    if not flag:
-        raise DigitError
-    # english PC
-    line1 = 'qwertyuiop'
-    line2 = 'asdfghjkl'
-    line3 = 'zxcvbnm'
-    for i in range(len(st) - 2):
-        elem = st[i: i + 3].lower()
-        if elem in line1:
-            raise SequenceError
-        elif elem in line2:
-            raise SequenceError
-        elif elem in line3:
-            raise SequenceError
-
-    # english mac
-    line1 = 'qwertzuiop'
-    line2 = 'asdfghjkl'
-    line3 = 'yxcvbnm'
-    for i in range(len(st) - 2):
-        elem = st[i: i + 3].lower()
-        if elem in line1:
-            raise SequenceError
-        elif elem in line2:
-            raise SequenceError
-        elif elem in line3:
-            raise SequenceError
-    # russian
-    line1 = 'йцукенгшщзхъ'
-    line2 = 'фывапролджэё'
-    line3 = 'ячсмитьбю'
-    for i in range(len(st) - 2):
-        elem = st[i: i + 3].lower()
-        if elem in line1:
-            raise SequenceError
-        elif elem in line2:
-            raise SequenceError
-        elif elem in line3:
-            raise SequenceError
-    return 'ok'
+print(main(stroka))
