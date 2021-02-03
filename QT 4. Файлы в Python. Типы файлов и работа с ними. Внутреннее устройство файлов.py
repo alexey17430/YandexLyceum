2e6427ecaa7810f675128ffa8b80ef7eb012ddf4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-03 20:12:47 +0500

    Перекодировщик

--- QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
+++ QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
@@ -1,8 +1,19 @@
-import random
+file = open('cyrillic.txt', mode='r', encoding='UTF-8')
+ans = open('transliteration.txt', mode='w')
+lines = file.read()
+dictionary = {"й": "j", "ц": "c", "у": "u", "к": "k", "е": "e", "н": "n",
+              "г": "g", "ш": "sh", "щ": "shh", "з": "z", "х": "h", "ъ": "#",
+              "ф": "f", "ы": "y", "в": "v", "а": "a", "п": "p", "р": "r",
+              "о": "o", "л": "l", "д": "d", "ж": "zh", "э": "je", "я": "ya",
+              "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "'",
+              "б": "b", "ю": "ju", "ё": "jo"}
+new_line = ''
+for letter in lines:
+    if letter in dictionary.keys():
+        new_line += dictionary[letter]
+    elif letter.lower() in dictionary.keys():
+        new_line += dictionary[letter.lower()].title()
+    else:
+        new_line += letter
 
-
-file = open('lines.txt')
-sp = list(map(lambda st: st.strip(), file.readlines()))
-if len(sp) > 0:
-    print(random.choice(sp))
-file.close()
+print(new_line, file=ans)
