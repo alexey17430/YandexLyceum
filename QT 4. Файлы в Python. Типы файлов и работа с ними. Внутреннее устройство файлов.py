b6988f90c5530efc6f7282ec68aeed7615bf6bc6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-03 20:13:15 +0500

    Переворот

--- QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
+++ QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
@@ -1,19 +1,7 @@
-file = open('cyrillic.txt', mode='r', encoding='UTF-8')
-ans = open('transliteration.txt', mode='w')
-lines = file.read()
-dictionary = {"й": "j", "ц": "c", "у": "u", "к": "k", "е": "e", "н": "n",
-              "г": "g", "ш": "sh", "щ": "shh", "з": "z", "х": "h", "ъ": "#",
-              "ф": "f", "ы": "y", "в": "v", "а": "a", "п": "p", "р": "r",
-              "о": "o", "л": "l", "д": "d", "ж": "zh", "э": "je", "я": "ya",
-              "ч": "ch", "с": "s", "м": "m", "и": "i", "т": "t", "ь": "'",
-              "б": "b", "ю": "ju", "ё": "jo"}
-new_line = ''
-for letter in lines:
-    if letter in dictionary.keys():
-        new_line += dictionary[letter]
-    elif letter.lower() in dictionary.keys():
-        new_line += dictionary[letter.lower()].title()
-    else:
-        new_line += letter
-
-print(new_line, file=ans)
+def reverse():
+    f = open('input.dat', 'rb')
+    data = f.read()
+    f.close()
+    f = open('output.dat', 'wb')
+    f.write(data[::-1])
+    f.close()
