be3d204062030470f7faa07d56e4eed5bb51e607
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-03 20:16:41 +0500

    Текстовый редактор 1.0

--- QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
+++ QT 4. Файлы в Python. Типы файлов и работа с ними. Внутреннее устройство файлов.py
@@ -1,4 +1,3 @@
-# имя файла со строками текста input.txt
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QLCDNumber
 from PyQt5.QtWidgets import QCheckBox, QPlainTextEdit
@@ -11,31 +10,58 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(600, 300, 350, 350)
-        self.setWindowTitle('Перемешивание')
+        self.setGeometry(600, 300, 450, 350)
+        self.setWindowTitle('Текстовый редактор')
         self.font = QtGui.QFont()
         self.font.setPointSize(14)
 
-        self.btn = QPushButton(self)
-        self.btn.resize(200, 50)
-        self.btn.move(25, 25)
-        self.btn.setText('Загрузить строки')
-        self.btn.setFont(self.font)
-        self.btn.clicked.connect(self.button_pushed)
+        self.line_edit = QLineEdit(self)
+        self.line_edit.move(25, 25)
+        self.line_edit.resize(150, 50)
+        self.line_edit.setFont(self.font)
 
-        self.edit = QPlainTextEdit(self)
-        self.edit.move(25, 100)
-        self.edit.resize(300, 225)
-        self.edit.setFont(self.font)
+        self.btn1 = QPushButton(self)
+        self.btn1.move(25, 100)
+        self.btn1.resize(150, 50)
+        self.btn1.setText('Создать новый')
+        self.btn1.setFont(self.font)
+        self.btn1.clicked.connect(self.button_pushed1)
 
-    def button_pushed(self):
-        file = open('input.txt')
-        lines = file.readlines()
-        for i in range(1, len(lines), 2):
-            self.edit.setPlainText(self.edit.toPlainText() + lines[i])
-        self.edit.setPlainText(self.edit.toPlainText() + '\n')
-        for i in range(0, len(lines), 2):
-            self.edit.setPlainText(self.edit.toPlainText() + lines[i])
+        self.btn2 = QPushButton(self)
+        self.btn2.move(25, 175)
+        self.btn2.resize(150, 50)
+        self.btn2.setText('Сохранить файл')
+        self.btn2.setFont(self.font)
+        self.btn2.clicked.connect(self.button_pushed2)
+
+        self.btn3 = QPushButton(self)
+        self.btn3.move(25, 250)
+        self.btn3.resize(150, 50)
+        self.btn3.setText('Открыть файл')
+        self.btn3.setFont(self.font)
+        self.btn3.clicked.connect(self.button_pushed3)
+
+        self.bloknot = QPlainTextEdit(self)
+        self.bloknot.move(200, 25)
+        self.bloknot.resize(225, 275)
+        self.bloknot.setFont(self.font)
+        self.bloknot.setDisabled(True)
+
+    def button_pushed1(self):
+        self.bloknot.setDisabled(False)
+        self.bloknot.setPlainText('')
+        self.line_edit.setText('')
+
+    def button_pushed2(self):
+        file = open(self.line_edit.text().strip(), 'w')
+        print(self.bloknot.toPlainText().strip(), file=file)
+        file.close()
+
+    def button_pushed3(self):
+        file = open(self.line_edit.text().strip())
+        self.bloknot.setPlainText(''.join(file.readlines()))
+        self.bloknot.setDisabled(False)
+        file.close()
 
 
 if __name__ == '__main__':
