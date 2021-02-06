36f9ab02c70096b01f032359dbbdb8b6b0d6da1e
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-06 16:13:47 +0500

    Самый дешевый путь

--- QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
+++ QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
@@ -1,21 +1,23 @@
 import csv
 
-file = open('rez.csv', encoding='UTF-8')
-reader = csv.DictReader(file, delimiter=',', quotechar='"')
-school, cls = map(int, input().split())
-ans = list()
+file = open('input.csv', encoding='UTF-8')
+reader = list(csv.reader(file, delimiter=';'))
+start_town = reader[-1][0]
+finish_town = reader[-1][1]
+del reader[-1]
+all_ways = dict()
 for elem in reader:
-    user_name = elem['user_name']
-    login = elem['login']
-    score = elem['Score']
-    login = login.split('-')
-    if int(login[-3]) == school and int(login[-2]) == cls:
-        ans.append([(user_name.split()[-2]), score])
-for i in range(len(ans) - 1):
-    for j in range(len(ans) - i - 1):
-        if int(ans[j][1]) > int(ans[j + 1][1]):
-            ans[j], ans[j + 1] = ans[j + 1], ans[j]
-        elif int(ans[j][1]) == int(ans[j + 1][1]) and (ans[j][0]) > (ans[j + 1][0]):
-            ans[j], ans[j + 1] = ans[j + 1], ans[j]
-for elem in reversed(ans):
-    print(*elem)
+    if (elem[0] == start_town and elem[1] == finish_town and (elem[0], elem[1]) not in all_ways) or\
+            elem[0] == start_town and elem[1] == finish_town and\
+            (elem[0], elem[1]) in all_ways and all_ways[(elem[0], elem[1])] < int(elem[2]):
+        all_ways[(elem[0], elem[1])] = int(elem[2])
+    else:
+        if elem[0] == start_town:
+            for next_elem in reader:
+                if next_elem[0] == elem[1] and next_elem[1] == finish_town:
+                    s = int(elem[2]) + int(next_elem[2])
+                    letters = (elem[0], next_elem[0], next_elem[1])
+                    if letters in all_ways and all_ways[letters] > s or letters not in all_ways:
+                        all_ways[letters] = s
+                        break
+print(*min(all_ways, key=lambda x: all_ways[x]))
