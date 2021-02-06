4cfb1eedeaa36be6aee25fde59ca6e7af4e053bd
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-06 16:12:13 +0500

    Осторожно: дорого

--- QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
+++ QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
@@ -1,6 +1,7 @@
 # имя файла - information.csv
 import sys
 import csv
+from random import randint
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem, QLineEdit
 from PyQt5 import QtGui
@@ -12,12 +13,12 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 500, 500)
+        self.setGeometry(300, 300, 500, 575)
         self.setWindowTitle('Интерактивный чек')
         font = QtGui.QFont()
         font.setPointSize(16)
         screen_font = QtGui.QFont()
-        screen_font.setPointSize(30)
+        screen_font.setPointSize(20)
 
         self.btn = QPushButton(self)
         self.btn.move(50, 425)
@@ -30,6 +31,13 @@ class Example(QWidget):
         self.edit.move(250, 425)
         self.edit.resize(200, 50)
         self.edit.setFont(screen_font)
+
+        self.btn_repaint = QPushButton(self)
+        self.btn_repaint.move(150, 500)
+        self.btn_repaint.resize(200, 50)
+        self.btn_repaint.setFont(screen_font)
+        self.btn_repaint.setText('Обновить')
+        self.btn_repaint.clicked.connect(self.button_repaint_pushed)
 
         file = open('information.csv', encoding='UTF-8')
         reader = list(csv.reader(file, delimiter=';'))
@@ -45,10 +53,11 @@ class Example(QWidget):
         self.table.setHorizontalHeaderItem(1, QTableWidgetItem('Цена'))
         self.table.setHorizontalHeaderItem(2, QTableWidgetItem('Кол-во'))
         self.table.setRowCount(len(list(reader)))
+        self.table.setAlternatingRowColors(True)
         self.reader = list(reader)
 
         n = 0
-        for elem in reader:
+        for elem in sorted(self.reader, key=lambda x: int(x[1]))[::-1]:
             name, cost = elem
             self.table.setItem(n, 0, QTableWidgetItem(name))
             self.table.setItem(n, 1, QTableWidgetItem(cost))
@@ -60,6 +69,15 @@ class Example(QWidget):
         for i in range(len(self.reader)):
             su += int(self.table.item(i, 1).text()) * int(self.table.item(i, 2).text())
         self.edit.setText(str(su))
+
+    def button_repaint_pushed(self):
+        for i in range(len(self.reader)):
+            if i == 5:
+                break
+            color = (randint(1, 255), randint(1, 255), randint(1, 255))
+            self.table.item(i, 0).setBackground(QtGui.QColor(*color))
+            self.table.item(i, 1).setBackground(QtGui.QColor(*color))
+            self.table.item(i, 2).setBackground(QtGui.QColor(*color))
 
 
 if __name__ == '__main__':
