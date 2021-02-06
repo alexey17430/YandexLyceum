c71ba847b8d407041e2d8276af11bdee66f9d729
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-06 16:14:22 +0500

    Результат олимпиады: фильтрация

--- QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
+++ QT 6. Работа с простыми таблицами (csv). Работа с табличными данными в PyQT.py
@@ -1,23 +1,74 @@
+import sys
 import csv
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit, QLCDNumber
+from PyQt5.QtWidgets import QCheckBox, QPlainTextEdit, QComboBox, QTableWidgetItem, QTableWidget
+from PyQt5.QtGui import QFont
 
-file = open('input.csv', encoding='UTF-8')
-reader = list(csv.reader(file, delimiter=';'))
-start_town = reader[-1][0]
-finish_town = reader[-1][1]
-del reader[-1]
-all_ways = dict()
-for elem in reader:
-    if (elem[0] == start_town and elem[1] == finish_town and (elem[0], elem[1]) not in all_ways) or\
-            elem[0] == start_town and elem[1] == finish_town and\
-            (elem[0], elem[1]) in all_ways and all_ways[(elem[0], elem[1])] < int(elem[2]):
-        all_ways[(elem[0], elem[1])] = int(elem[2])
-    else:
-        if elem[0] == start_town:
-            for next_elem in reader:
-                if next_elem[0] == elem[1] and next_elem[1] == finish_town:
-                    s = int(elem[2]) + int(next_elem[2])
-                    letters = (elem[0], next_elem[0], next_elem[1])
-                    if letters in all_ways and all_ways[letters] > s or letters not in all_ways:
-                        all_ways[letters] = s
-                        break
-print(*min(all_ways, key=lambda x: all_ways[x]))
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(600, 300, 600, 600)
+        self.setWindowTitle('Результат олимпиады: фильтрация')
+        self.font = QFont()
+        self.font.setPointSize(12)
+        file = open('rez.csv', encoding='UTF-8')
+        self.reader = list(csv.DictReader(file, delimiter=',', quotechar='"'))
+        self.schools = set()
+        self.classes = set()
+        for elem in self.reader:
+            self.schools.add(elem['login'].split('-')[2])
+            self.classes.add(elem['login'].split('-')[-2])
+        self.box1 = QComboBox(self)
+        self.box1.move(30, 25)
+        self.box1.resize(160, 25)
+        self.box1.setFont(self.font)
+        self.box1.addItem('Все')
+        for elem in self.schools:
+            self.box1.addItem(elem)
+
+        self.box2 = QComboBox(self)
+        self.box2.move(220, 25)
+        self.box2.resize(160, 25)
+        self.box2.setFont(self.font)
+        self.box2.addItem('Все')
+        for elem in self.classes:
+            self.box2.addItem(elem)
+
+        self.btn = QPushButton(self)
+        self.btn.move(410, 25)
+        self.btn.resize(160, 25)
+        self.btn.setFont(self.font)
+        self.btn.setText('Узнать результаты')
+        self.btn.clicked.connect(self.btn_pushed)
+
+        self.table = QTableWidget(self)
+        self.table.move(25, 75)
+        self.table.resize(550, 500)
+        self.table.setColumnCount(2)
+        self.table.setHorizontalHeaderItem(0, QTableWidgetItem('Фамилия'))
+        self.table.setHorizontalHeaderItem(1, QTableWidgetItem('Результат'))
+
+    def btn_pushed(self):
+        school = self.box1.currentText()
+        cls = self.box2.currentText()
+        ans = list()
+        for elem in self.reader:
+            if (school == 'Все' or school == elem['login'].split('-')[2]) and \
+                    (cls == 'Все' or cls == elem['login'].split('-')[-2]):
+                ans.append((elem['user_name'].split()[3], elem['Score']))
+        self.table.setRowCount(len(ans))
+        for i in range(len(ans)):
+            elem = ans[i]
+            self.table.setItem(i, 0, QTableWidgetItem(elem[0]))
+            self.table.setItem(i, 1, QTableWidgetItem(elem[1]))
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
