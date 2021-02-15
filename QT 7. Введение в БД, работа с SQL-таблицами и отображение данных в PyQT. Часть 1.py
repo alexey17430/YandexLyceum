6e763b7eaec756f38d7c1cd47d47074e574e6175
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-15 19:37:50 +0500

    Популярные жанры

--- QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
+++ QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
@@ -3,7 +3,12 @@ import sqlite3
 file_name = input()
 con = sqlite3.connect(file_name)
 cur = con.cursor()
-result = cur.execute(f'''SELECT title FROM films WHERE
-genre = (SELECT id from genres where title = 'детектив') and 1995 <= year and year <= 2000''').fetchall()
+
+result = cur.execute(f'''select title from genres where id in 
+(select genre from films where 2010 <= year and year <= 2011)''')
+ans = list()
 for elem in result:
+    if elem not in ans:
+        ans.append(elem)
+for elem in ans:
     print(elem[0])
