db6c2134ea1d32fedbe50ddaaef9bdfc413b8108
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-15 19:38:22 +0500

    Астерикс без Обеликса

--- QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
+++ QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
@@ -4,7 +4,8 @@ file_name = input()
 con = sqlite3.connect(file_name)
 cur = con.cursor()
 
-result = cur.execute(f'''select title from films where duration <= 85''')
+result = cur.execute(f'''select title from films where title like "%Астерикс%"
+and not title like "%Обеликс%"''')
 
 for elem in result:
     print(elem[0])
