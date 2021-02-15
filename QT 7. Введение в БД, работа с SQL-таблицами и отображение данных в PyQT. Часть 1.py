b0d1056c7d82e3d2b37d4e76d5113f045b52f404
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-15 19:36:25 +0500

    Вопросы на конце

--- QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
+++ QT 7. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 1.py
@@ -3,9 +3,6 @@ import sqlite3
 file_name = input()
 con = sqlite3.connect(file_name)
 cur = con.cursor()
-result = cur.execute(f'''
-SELECT title FROM films
-    WHERE genre in (select id from genres
-    where year >= 1997 AND (title = 'музыка' or title = 'анимация'))''').fetchall()
+result = cur.execute(f'''SELECT title FROM films WHERE title like "%?"''').fetchall()
 for elem in result:
     print(elem[0])
