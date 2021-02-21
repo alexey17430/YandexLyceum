a4b99f6ed05396345014c68f1b74b222c4c234db
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-02-21 16:30:25 +0500

    У любого фильма есть длина

--- QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2.py
+++ QT 8. Введение в БД, работа с SQL-таблицами и отображение данных в PyQT. Часть 2.py
@@ -4,12 +4,9 @@ import sqlite3
 def get_result(name):
     con = sqlite3.connect(name)
     cur = con.cursor()
-    id = cur.execute('''
-    select id from main.genres
-    where title = "комедия"
-    ''').fetchone()[0]
-    cur.execute(f'''
-    delete from films
-    where genre = {id}
+    cur.execute('''
+    UPDATE films
+    SET duration = 42
+    WHERE duration = ""
     ''')
     con.commit()
