b399fe24f8159f1a8ac4ff4c738eb2fe43578159
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:23:05 +0500

    Сфера

--- PyGame 1. Введение.py
+++ PyGame 1. Введение.py
@@ -1,27 +1,29 @@
 import pygame
 
 
-def draw(w, n):
-    screen.fill((0, 0, 0))
-    sp = ['red', 'green', 'blue']
-    x, y = w * n, w * n
+def draw(n):
+    pi = 3.14
+    screen.fill(color=pygame.Color('black'))
+    dx = 150 / n
     for i in range(n):
-        color = sp[i % 3]
-        pygame.draw.circle(surface=screen, center=(x, y), radius=w + i * w,
-                           color=pygame.Color(color), width=w)
+        pygame.draw.ellipse(screen, pygame.Color('white'), (dx * i, 0, 300 - dx * i * 2, 300),
+                            width=1)
+    for j in range(n):
+        pygame.draw.ellipse(screen, pygame.Color('white'), (0, dx * j, 300, 300 - dx * j * 2),
+                            width=1)
 
 
 if __name__ == '__main__':
     pygame.init()
     try:
-        width, colvo = map(int, input().split())
-        size = (width * colvo * 2, width * colvo * 2)
+        n = int(input())
+        size = (300, 300)
         screen = pygame.display.set_mode(size)
-        draw(width, colvo)
+        draw(n)
     except:
         print('Введены данные в неверном формате')
         exit(0)
-    pygame.display.set_caption('Мишень')
+    pygame.display.set_caption('Сфера')
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
