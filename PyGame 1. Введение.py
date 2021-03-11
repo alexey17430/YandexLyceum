39e0fd074b0f5ac3aa8a7300cae2d9e783bf2dc5
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:21:34 +0500

    Мишень

--- PyGame 1. Введение.py
+++ PyGame 1. Введение.py
@@ -1,37 +1,27 @@
 import pygame
 
 
-def draw(width, colvo):
+def draw(w, n):
     screen.fill((0, 0, 0))
-    sqr_width = width / colvo
-    color_start = 'black'
-    for i in range(colvo):
-        color = color_start
-        for j in range(colvo):
-            if color == 'white':
-                pygame.draw.rect(screen, pygame.Color('white'), (
-                width - sqr_width * (colvo - j), width - sqr_width * (i + 1),
-                sqr_width, sqr_width), 0)
-                color = 'black'
-            else:
-                color = 'white'
-        if color_start == 'black':
-            color_start = 'white'
-        else:
-            color_start = 'black'
+    sp = ['red', 'green', 'blue']
+    x, y = w * n, w * n
+    for i in range(n):
+        color = sp[i % 3]
+        pygame.draw.circle(surface=screen, center=(x, y), radius=w + i * w,
+                           color=pygame.Color(color), width=w)
 
 
 if __name__ == '__main__':
     pygame.init()
     try:
-        a, n = map(int, input().split())
-        size = (a, a)
+        width, colvo = map(int, input().split())
+        size = (width * colvo * 2, width * colvo * 2)
         screen = pygame.display.set_mode(size)
-        draw(a, n)
+        draw(width, colvo)
     except:
         print('Введены данные в неверном формате')
         exit(0)
-    pygame.display.set_caption('Шахматная клетка')
+    pygame.display.set_caption('Мишень')
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
