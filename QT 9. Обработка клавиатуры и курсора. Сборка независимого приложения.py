01eb48b56ccde11487eb92ac2eb35fef764622e3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:16:41 +0500

    Машинка

--- QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения.py
+++ QT 9. Обработка клавиатуры и курсора. Сборка независимого приложения.py
@@ -1,8 +1,8 @@
 import sys
-from random import randint
 from PyQt5.QtWidgets import QWidget, QApplication, QLabel
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtCore import Qt, QPoint
+from random import randint
 
 
 class Example(QWidget):
@@ -12,55 +12,37 @@ class Example(QWidget):
 
     def initUI(self):
         self.setGeometry(600, 300, 500, 500)
-        self.setWindowTitle('Супрематизм')
-        self.button = ''
-        self.coords = tuple()
-        self.coords_space = tuple()
+        self.setWindowTitle('Машинка')
+        self.x, self.y = 0, 0
         self.setMouseTracking(True)
+        self.color1 = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+        self.color2 = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
 
-    def mousePressEvent(self, event):
-        self.coords = (event.x(), event.y())
-        if event.button() == Qt.LeftButton:
-            self.button = 'left'
-            self.update()
-        else:
-            self.button = 'right'
-            self.update()
+    def mouseMoveEvent(self, event):
+        self.x, self.y = event.x(), event.y()
+        self.update()
+
+    def paintEvent(self, event):
+        qp = QPainter()
+        qp.begin(self)
+        qp.setBrush(self.color1)
+        qp.drawRect(self.x - 50, self.y - 50, 100, 100)
+        qp.drawEllipse(self.x - 25, self.y - 100, 50, 50)
+        qp.setBrush(self.color2)
+        qp.drawEllipse(self.x - 80, self.y - 25, 30, 30)
+        qp.drawEllipse(self.x + 50, self.y - 25, 30, 30)
+        qp.drawEllipse(self.x - 50, self.y + 50, 30, 30)
+        qp.drawEllipse(self.x + 20, self.y + 50, 30, 30)
+        qp.setBrush(QColor(255, 91, 0))
+        qp.drawEllipse(self.x - 7, self.y - 70, 15, 15)
+        qp.drawEllipse(self.x - 15, self.y - 90, 10, 10)
+        qp.drawEllipse(self.x + 15, self.y - 90, 10, 10)
+        qp.end()
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Space:
-            self.button = 'space'
-            self.update()
-
-    def mouseMoveEvent(self, event):
-        self.coords_space = (event.x(), event.y())
-
-    def paintEvent(self, event):
-        if self.button == 'left':
-            qp = QPainter()
-            qp.begin(self)
-            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-            r = randint(50, 350)
-            qp.drawEllipse(self.coords[0] - (r // 4), self.coords[1] - (r // 4), r // 2, r // 2)
-            qp.end()
-        elif self.button == 'right':
-            qp = QPainter()
-            qp.begin(self)
-            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-            a = randint(15, 200)
-            b = randint(15, 200)
-            qp.drawRect(self.coords[0] - a // 2, self.coords[1] - b // 2, a, b)
-            qp.end()
-        elif self.button == 'space':
-            qp = QPainter()
-            qp.begin(self)
-            qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
-            a = randint(15, 200)
-            x, y = self.coords_space
-            qp.drawPolygon(QPoint(x - a // 2, y + a // 2),
-                           QPoint(x + a // 2, y + a // 2),
-                           QPoint(x, y - a // 2))
-            qp.end()
+            self.color1 = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
+            self.color2 = QColor(randint(0, 255), randint(0, 255), randint(0, 255))
 
 
 if __name__ == '__main__':
