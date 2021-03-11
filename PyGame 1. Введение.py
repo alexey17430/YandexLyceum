55d7d23530bc00fad3942ebe8f8ef2a9a993fb12
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:20:24 +0500

    Прямоугольник

--- PyGame 1. Введение.py
+++ PyGame 1. Введение.py
@@ -3,22 +3,21 @@ import pygame
 
 def draw(w, h):
     screen.fill((0, 0, 0))
-    pygame.draw.line(screen, pygame.Color(255, 255, 255), (0, 0), (w, h), 5)
-    pygame.draw.line(screen, pygame.Color(255, 255, 255), (w, 0), (0, h), 5)
+    pygame.draw.rect(screen, pygame.Color(255, 0, 0), (2, 2, w - 3, h - 3), 0)
 
 
 if __name__ == '__main__':
     try:
-        w, h = map(int, input().split())
+        width, height = map(int, input().split())
         pygame.init()
-        size = (w, h)
+        size = (width, height)
         screen = pygame.display.set_mode(size)
-        draw(w, h)
+        draw(width, height)
+        pygame.display.set_caption('Прямоугольник')
+        pygame.display.flip()
+        while pygame.event.wait().type != pygame.QUIT:
+            pass
+        pygame.quit()
     except:
         print('Неверный формат введённых данных')
         exit(0)
-    pygame.display.set_caption('Крест')
-    pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
-    pygame.quit()
