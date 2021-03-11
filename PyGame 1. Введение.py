c0a2204199870f3f450bf59c5c871896300a54e3
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:23:34 +0500

    Кирпичи

--- PyGame 1. Введение.py
+++ PyGame 1. Введение.py
@@ -1,29 +1,35 @@
 import pygame
 
 
-def draw(n):
-    screen.fill(color=pygame.Color('yellow'))
-    for i in range(300 // n):
-        for j in range(300 // n):
-            pygame.draw.polygon(screen, color=pygame.Color('orange'),
-                                points=((j * n, i * n + n / 2),
-                                        (j * n + n / 2, i * n + n),
-                                        (j * n + n, i * n + n / 2),
-                                        (j * n + n / 2, i * n),
-                                        (j * n, i * n + n / 2)))
+def draw():
+    screen.fill(color=pygame.Color('black'))
+    x, y = 0, 0
+    for i in range(12):
+        for j in range(10):
+            if i % 2 != 0 and j == 0:
+                pygame.draw.rect(screen, pygame.Color('red'), (x, y, 15, 15))
+                pygame.draw.rect(screen, pygame.Color('white'), (x + 15, y, 2, 15))
+                x += 17
+            else:
+                pygame.draw.rect(screen, pygame.Color('red'), (x, y, 30, 15))
+                pygame.draw.rect(screen, pygame.Color('white'), (x + 30, y, 2, 15))
+                x += 32
+
+        pygame.draw.rect(screen, pygame.Color('white'), (0, y + 15, 300, 2))
+        y += 17
+        x = 0
 
 
 if __name__ == '__main__':
     pygame.init()
     try:
-        n = int(input())
-        size = (300, 300)
+        size = (300, 200)
         screen = pygame.display.set_mode(size)
-        draw(n)
+        draw()
     except:
         print('Введены данные в неверном формате')
         exit(0)
-    pygame.display.set_caption('Ромбики')
+    pygame.display.set_caption('Кирпичи')
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
