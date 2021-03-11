be42f9244e396cc46137936efce19fb2b11b8b9f
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-11 20:23:50 +0500

    Куб

--- PyGame 1. Введение.py
+++ PyGame 1. Введение.py
@@ -1,35 +1,58 @@
 import pygame
 
 
-def draw():
+def draw(w, hue):
     screen.fill(color=pygame.Color('black'))
-    x, y = 0, 0
-    for i in range(12):
-        for j in range(10):
-            if i % 2 != 0 and j == 0:
-                pygame.draw.rect(screen, pygame.Color('red'), (x, y, 15, 15))
-                pygame.draw.rect(screen, pygame.Color('white'), (x + 15, y, 2, 15))
-                x += 17
-            else:
-                pygame.draw.rect(screen, pygame.Color('red'), (x, y, 30, 15))
-                pygame.draw.rect(screen, pygame.Color('white'), (x + 30, y, 2, 15))
-                x += 32
 
-        pygame.draw.rect(screen, pygame.Color('white'), (0, y + 15, 300, 2))
-        y += 17
-        x = 0
+    color = pygame.Color(0, 0, 0)
+    hsv = color.hsva
+    color.hsva = (hue, 100, 75, hsv[3])
+    x = 150 - (w + w // 2) // 2
+    y = (300 - w - w // 2) // 2 + w // 2
+    pygame.draw.rect(screen, color, rect=(x, y, w, w))
+
+    color = pygame.Color(0, 0, 0)
+    hsv = color.hsva
+    color.hsva = (hue, 100, 100, hsv[3])
+    x1 = (150 - (w + w // 2) // 2) + w // 2
+    y1 = (300 - w - w // 2) // 2
+    x2 = (150 - (w + w // 2) // 2) + w // 2 + w
+    y2 = (300 - w - w // 2) // 2
+    x3 = (150 - (w + w // 2) // 2) + w
+    y3 = (300 - w - w // 2) // 2 + w // 2
+    x4 = 150 - (w + w // 2) // 2
+    y4 = (300 - w - w // 2) // 2 + w // 2
+    pygame.draw.polygon(screen, color, ((x1, y1),
+                                        (x2, y2),
+                                        (x3, y3),
+                                        (x4, y4)))
+
+    color = pygame.Color(0, 0, 0)
+    hsv = color.hsva
+    color.hsva = (hue, 100, 50, hsv[3])
+    x1 = (150 - (w + w // 2) // 2) + w
+    y1 = (300 - w - w // 2) // 2 + w // 2 + w
+    x4 = (150 - (w + w // 2) // 2) + w + w // 2
+    y4 = (300 - w - w // 2) // 2 + w
+    pygame.draw.polygon(screen, color, ((x2, y2),
+                                        (x3, y3),
+                                        (x1, y1),
+                                        (x4, y4)))
 
 
 if __name__ == '__main__':
     pygame.init()
     try:
-        size = (300, 200)
+        w, hue = map(int, input().split())
+        size = (300, 300)
+        if w > 150 or w % 4 != 0 or not 0 <= hue <= 100:
+            raise IndexError
         screen = pygame.display.set_mode(size)
-        draw()
+        draw(w, hue)
     except:
         print('Введены данные в неверном формате')
         exit(0)
-    pygame.display.set_caption('Кирпичи')
+    pygame.display.set_caption('Куб')
     pygame.display.flip()
     while pygame.event.wait().type != pygame.QUIT:
         pass
