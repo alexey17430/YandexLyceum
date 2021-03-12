df958d263264be5f483d7b1647ca6d034a49e680
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-12 20:42:34 +0500

    Перетаскивание

--- PyGame 2. Игровой цикл. События.py
+++ PyGame 2. Игровой цикл. События.py
@@ -1,52 +1,39 @@
 import pygame
-from random import randint
-
-WHITE = (255, 255, 255)
-
-
-class Ball:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.vx = randint(-10, 10)
-        self.vy = randint(-10, 10)
-
-    def move(self):
-        self.x += self.vx
-        self.y += self.vy
-        if self.x < 10 or self.x > width - 10:
-            self.vx *= -1
-        if self.y < 10 or self.y > height - 10:
-            self.vy *= -1
-
-    def coords(self):
-        return self.x, self.y
 
 
 if __name__ == '__main__':
     pygame.init()
-    pygame.display.set_caption('Шарики')
-    size = width, height = 800, 400
+    pygame.display.set_caption('Перетаскивание')
+    size = width, height = 300, 300
     screen = pygame.display.set_mode(size)
-    balls = []
     running = True
     v = 100
     fps = 60
     clock = pygame.time.Clock()
+    clicked = False
+    rx, ry = 0, 0
+    w, h = 100, 100
+    pygame.draw.rect(screen, pygame.Color('green'), (rx, ry, w, h))
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
-                    ball = Ball(*event.pos)
-                    balls.append(ball)
+                    mx, my = event.pos
+                    if rx <= mx <= rx + w and ry <= my <= ry + h and not clicked:
+                        clicked = True
+                        dx = mx - rx
+                        dy = my - ry
 
-        screen.fill((0, 0, 0))
-        for ball in balls:
-
-            pygame.draw.circle(screen, WHITE, ball.coords(), 10)
-            ball.move()
+            if event.type == pygame.MOUSEMOTION and clicked:
+                screen.fill((0, 0, 0))
+                mx, my = event.pos
+                rx, ry = mx - dx, my - dy
+                pygame.draw.rect(screen, pygame.Color('green'), (rx, ry, w, h))
+            if event.type == pygame.MOUSEBUTTONUP:
+                if event.button == 1:
+                    clicked = False
         clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
