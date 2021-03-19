cd3e76bb4088e583b06602e15095ebb1aa8fe255
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-19 19:13:40 +0500

    Десант

--- PyGame 6. Столкновения и другие взаимодействия.py
+++ PyGame 6. Столкновения и другие взаимодействия.py
@@ -1,57 +1,64 @@
 import random
 import pygame
+import os
+import sys
 
 
-class Ball(pygame.sprite.Sprite):
-    def __init__(self, radius, x, y):
+size = width, height = 800, 600
+screen = pygame.display.set_mode(size)
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+class Mountain(pygame.sprite.Sprite):
+    image = load_image("mountains.png")
+
+    def __init__(self):
         super().__init__(all_sprites)
-        self.radius = radius
-        self.image = pygame.Surface((2 * radius, 2 * radius),
-                                    pygame.SRCALPHA, 32)
-        pygame.draw.circle(self.image, pygame.Color("red"),
-                           (radius, radius), radius)
-        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
-        self.vx = random.randint(-10, 10)
-        self.vy = random.randint(-10, 10)
+        self.image = Mountain.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.bottom = height
+
+
+class Landing(pygame.sprite.Sprite):
+    image = load_image("pt.png")
+
+    def __init__(self, pos):
+        super().__init__(all_sprites)
+        self.image = Landing.image
+        self.rect = self.image.get_rect()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect.x = pos[0]
+        self.rect.y = pos[1]
 
     def update(self):
-        self.rect = self.rect.move(self.vx, self.vy)
-        if pygame.sprite.spritecollideany(self, horizontal_borders):
-            self.vy = -self.vy
-        if pygame.sprite.spritecollideany(self, vertical_borders):
-            self.vx = -self.vx
-
-
-class Border(pygame.sprite.Sprite):
-    def __init__(self, x1, y1, x2, y2):
-        super().__init__(all_sprites)
-        if x1 == x2:
-            self.add(vertical_borders)
-            self.image = pygame.Surface([1, y2 - y1])
-            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
-        else:
-            self.add(horizontal_borders)
-            self.image = pygame.Surface([x2 - x1, 1])
-            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+        if not pygame.sprite.collide_mask(self, mountain):
+            self.rect = self.rect.move(0, 1)
 
 
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
     pygame.display.set_caption('Balls')
     horizontal_borders = pygame.sprite.Group()
     vertical_borders = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
-
-    Border(5, 5, width - 5, 5)
-    Border(5, height - 5, width - 5, height - 5)
-    Border(5, 5, 5, height - 5)
-    Border(width - 5, 5, width - 5, height - 5)
-
-    for i in range(50):
-        Ball(20, 400, 300)
-
+    mountain = Mountain()
     clock = pygame.time.Clock()
     fps = 60
     running = True
@@ -59,7 +66,9 @@ if __name__ == '__main__':
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        screen.fill(pygame.Color('white'))
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                Landing(event.pos)
+        screen.fill(pygame.Color('black'))
         all_sprites.draw(screen)
         all_sprites.update()
         clock.tick(fps)
