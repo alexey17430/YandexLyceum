56ab1458f55a99d34ec0c16da521738221878adf
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-19 19:12:45 +0500

    Пересечение прямоугольников «вручную»

--- PyGame 6. Столкновения и другие взаимодействия.py
+++ PyGame 6. Столкновения и другие взаимодействия.py
@@ -1,7 +1,10 @@
-x1, y1, r1 = map(int, input().split())
-x2, y2, r2 = map(int, input().split())
-s = ((x1 - x2)**2 + (y1 - y2)**2)**0.5
-if s <= r1 + r2:
+x1, y1, w1, h1 = map(int, input().split())
+x2, y2, w2, h2 = map(int, input().split())
+
+if x2 <= x1 <= x2 + w2 or \
+        y2 <= y1 <= y2 + h2 and \
+        x1 <= x2 <= x1 + w1 or \
+        y1 <= y2 <= y1 + h1:
     print('YES')
 else:
     print('NO')
