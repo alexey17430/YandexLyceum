539c5531dda9628fc5fb1476de83ccc395cc6887
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-19 19:13:09 +0500

    Столкновение шариков

--- PyGame 6. Столкновения и другие взаимодействия.py
+++ PyGame 6. Столкновения и другие взаимодействия.py
@@ -1,10 +1,67 @@
-x1, y1, w1, h1 = map(int, input().split())
-x2, y2, w2, h2 = map(int, input().split())
+import random
+import pygame
 
-if x2 <= x1 <= x2 + w2 or \
-        y2 <= y1 <= y2 + h2 and \
-        x1 <= x2 <= x1 + w1 or \
-        y1 <= y2 <= y1 + h1:
-    print('YES')
-else:
-    print('NO')
+
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, radius, x, y):
+        super().__init__(all_sprites)
+        self.radius = radius
+        self.image = pygame.Surface((2 * radius, 2 * radius),
+                                    pygame.SRCALPHA, 32)
+        pygame.draw.circle(self.image, pygame.Color("red"),
+                           (radius, radius), radius)
+        self.rect = pygame.Rect(x, y, 2 * radius, 2 * radius)
+        self.vx = random.randint(-10, 10)
+        self.vy = random.randint(-10, 10)
+
+    def update(self):
+        self.rect = self.rect.move(self.vx, self.vy)
+        if pygame.sprite.spritecollideany(self, horizontal_borders):
+            self.vy = -self.vy
+        if pygame.sprite.spritecollideany(self, vertical_borders):
+            self.vx = -self.vx
+
+
+class Border(pygame.sprite.Sprite):
+    def __init__(self, x1, y1, x2, y2):
+        super().__init__(all_sprites)
+        if x1 == x2:
+            self.add(vertical_borders)
+            self.image = pygame.Surface([1, y2 - y1])
+            self.rect = pygame.Rect(x1, y1, 1, y2 - y1)
+        else:
+            self.add(horizontal_borders)
+            self.image = pygame.Surface([x2 - x1, 1])
+            self.rect = pygame.Rect(x1, y1, x2 - x1, 1)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    size = width, height = 800, 600
+    screen = pygame.display.set_mode(size)
+    pygame.display.set_caption('Balls')
+    horizontal_borders = pygame.sprite.Group()
+    vertical_borders = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
+
+    Border(5, 5, width - 5, 5)
+    Border(5, height - 5, width - 5, height - 5)
+    Border(5, 5, 5, height - 5)
+    Border(width - 5, 5, width - 5, height - 5)
+
+    for i in range(50):
+        Ball(20, 400, 300)
+
+    clock = pygame.time.Clock()
+    fps = 60
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        screen.fill(pygame.Color('white'))
+        all_sprites.draw(screen)
+        all_sprites.update()
+        clock.tick(fps)
+        pygame.display.flip()
+    pygame.quit()
