794697822914a81f494e735ae7c45e984717b45d
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:39:24 +0500

    Сложение многочленов

--- ООП. Определение операторов.py
+++ ООП. Определение операторов.py
@@ -1,9 +1,20 @@
-class SparseArray:
-    def __init__(self):
-        self.dict_of_numbers = dict()
+class Polynomial:
+    def __init__(self, coefficients):
+        self.coefficients = dict()
+        for i in range(len(coefficients)):
+            self.coefficients[i] = [coefficients[i]]
 
-    def __getitem__(self, key):
-        return self.dict_of_numbers.get(key, 0)
+    def __call__(self, x):
+        answer = 0
+        for key, values in self.coefficients.items():
+            for value in values:
+                answer += value * (x ** key)
+        return answer
 
-    def __setitem__(self, key, value):
-        self.dict_of_numbers[key] = value
+    def __add__(self, other):
+        new_coefficients = dict()
+        dlina = len(self.coefficients) if len(self.coefficients) >= len(other.coefficients)\
+            else len(other.coefficients)
+        for i in range(dlina):
+            new_coefficients[i] = self.coefficients.get(i, [0]) + (other.coefficients.get(i, [0]))
+        return Polynomial(list(sum(value)for key, value in new_coefficients.items()))
