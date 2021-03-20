c58e4ebfe57e7761da44be5d20197a0c9333a3b6
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:35:15 +0500

    Вывод предложений

--- Введение в ООП. Полиморфизм.py
+++ Введение в ООП. Полиморфизм.py
@@ -1,9 +1,22 @@
-class Selector:
-    def __init__(self, number):
-        self.number = number.copy()
+class LeftParagraph:
+    def __init__(self, width):
+        self.width = width
+        self.all_words = list()
 
-    def get_odds(self):
-        return list(filter(lambda x: x % 2 != 0, self.number))
+    def add_word(self, word):
+        if self.all_words and len(word) + len(self.all_words[-1]) < self.width:
+            self.all_words[-1] += ' ' + word
+        else:
+            self.all_words.append(word)
 
-    def get_evens(self):
-        return list(filter(lambda x: x % 2 == 0, self.number))
+    def end(self):
+        for elem in self.all_words:
+            print(elem)
+        self.all_words = list()
+
+
+class RightParagraph(LeftParagraph):
+    def end(self):
+        for elem in self.all_words:
+            print(elem.rjust(self.width, ' '))
+        self.all_words = list()
