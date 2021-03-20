5ba253d0afe9897a01203a4b2f4d1610098038d8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:35:45 +0500

    Статистика

--- Введение в ООП. Полиморфизм.py
+++ Введение в ООП. Полиморфизм.py
@@ -1,60 +1,19 @@
-class AmericanDate:
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
+class MinStat:
+    def __init__(self):
+        self.all_numbers = list()
 
-    def set_year(self, chislo):
-        self.year = chislo
+    def add_number(self, chislo):
+        self.all_numbers.append(chislo)
 
-    def set_month(self, chislo):
-        self.month = chislo
-
-    def set_day(self, chislo):
-        self.day = chislo
-
-    def get_year(self):
-        return self.year
-
-    def get_month(self):
-        return self.month
-
-    def get_day(self):
-        return self.day
-
-    def format(self):
-        month = self.month if self.month >= 10 else '0' + str(self.month)
-        day = self.day if self.day >= 10 else '0' + str(self.day)
-        year = self.year
-        return f'{month}.{day}.{year}'
+    def result(self):
+        return min(self.all_numbers) if self.all_numbers else None
 
 
-class EuropeanDate:
-    def __init__(self, year, month, day):
-        self.year = year
-        self.month = month
-        self.day = day
+class MaxStat(MinStat):
+    def result(self):
+        return max(self.all_numbers) if self.all_numbers else None
 
-    def set_year(self, chislo):
-        self.year = chislo
 
-    def set_month(self, chislo):
-        self.month = chislo
-
-    def set_day(self, chislo):
-        self.day = chislo
-
-    def get_year(self):
-        return self.year
-
-    def get_month(self):
-        return self.month
-
-    def get_day(self):
-        return self.day
-
-    def format(self):
-        month = self.month if self.month >= 10 else '0' + str(self.month)
-        day = self.day if self.day >= 10 else '0' + str(self.day)
-        year = self.year
-        return f'{day}.{month}.{year}'
+class AverageStat(MinStat):
+    def result(self):
+        return sum(self.all_numbers) / len(self.all_numbers) if self.all_numbers else None
