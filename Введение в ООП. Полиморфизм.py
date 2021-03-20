ad717b66b78844ea1c01a856174969f4846f41b8
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:36:14 +0500

    Прямоугольники

--- Введение в ООП. Полиморфизм.py
+++ Введение в ООП. Полиморфизм.py
@@ -1,32 +1,48 @@
-class Table:
-    """
-    rows - количество списков внутри списка
-    cols - длина маленьких списков
-    """
-    def __init__(self, rows, cols):
-        self.table = list()
-        for i in range(rows):
-            small_ans = list()
-            for j in range(cols):
-                small_ans.append(0)
-            self.table.append(small_ans)
-        self.rows = rows
-        self.cols = cols
+class Rectangle:
+    def __init__(self, x, y, w, h):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + w
+        self.y2 = y + h
 
-    def get_value(self, row, col):
-        try:
-            ans = self.table[row][col]
-        except IndexError:
-            ans = None
-        if row < 0 or col < 0:
-            ans = None
-        return ans
+    def intersection(self, other):
+        if self.x2 <= other.x1 or self.x1 >= other.x2:
+            return None
+        elif self.y1 >= other.y2 or self.y2 <= other.y1:
+            return None
+        else:
+            answer = list()  # x1, x2, y1, y2
+            if self.x1 < other.x1:
+                answer.append(other.x1)
+            else:
+                answer.append(self.x1)
 
-    def set_value(self, row, col, value):
-        self.table[row][col] = value
+            if self.x2 < other.x2:
+                answer.append(self.x2)
+            else:
+                answer.append(other.x2)
 
-    def n_rows(self):
-        return len(self.table)
+            if self.y1 < other.y1:
+                answer.append(other.y1)
+            else:
+                answer.append(self.y1)
 
-    def n_cols(self):
-        return len(self.table[0]) if self.table else 0
+            if self.y2 < other.y2:
+                answer.append(self.y2)
+            else:
+                answer.append(other.y2)
+
+            return Rectangle(min(answer[0], answer[1]), min(answer[2], answer[3]),
+                             abs(answer[0] - answer[1]), abs(answer[2] - answer[3]))
+
+    def get_x(self):
+        return self.x1
+
+    def get_y(self):
+        return self.y1
+
+    def get_w(self):
+        return self.x2 - self.x1
+
+    def get_h(self):
+        return self.y2 - self.y1
