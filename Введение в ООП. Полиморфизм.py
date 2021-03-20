fe7373bdac80caec3f3a26cbad231b9dacad40d4
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:36:50 +0500

    СМС-рассылка

--- Введение в ООП. Полиморфизм.py
+++ Введение в ООП. Полиморфизм.py
@@ -1,76 +1,49 @@
-class Table:
-    """
-    rows - количество списков внутри списка
-    cols - длина маленьких списков
-    """
+class Person:
+    def __init__(self, name, second_name, family_name, phones_numbers):
+        self.name = name
+        self.second_name = second_name
+        self.family_name = family_name
+        self.phones_numbers = phones_numbers
 
-    def __init__(self, rows, cols):
-        self.table = list()
-        for i in range(rows):
-            small_ans = list()
-            for j in range(cols):
-                small_ans.append(0)
-            self.table.append(small_ans)
-        self.rows = rows
-        self.cols = cols
+    def get_phone(self):
+        return self.phones_numbers.get('private', None)
 
-    def get_value(self, row, col):
-        try:
-            ans = self.table[row][col]
-        except IndexError:
-            ans = None
-        if row < 0 or col < 0:
-            ans = None
-        return ans
+    def get_name(self):
+        return ' '.join([self.family_name, self.name, self.second_name])
 
-    def set_value(self, row, col, value):
-        self.table[row][col] = value
+    def get_work_phone(self):
+        return self.phones_numbers.get('work', None)
 
-    def n_rows(self):
-        return len(self.table)
+    def get_sms_text(self):
+        return f'Уважаемый {self.name} {self.second_name}! Примите участие в нашем ' \
+            f'беспроигрышном конкурсе для физических лиц'
 
-    def n_cols(self):
-        return len(self.table[0]) if self.table else 0
 
-    def delete_row(self, row):
-        new_table = [self.table[i] for i in range(len(self.table)) if i != row]
-        self.table.clear()
-        self.table.extend(new_table)
+class Company:
+    def __init__(self, name_of_company, type_of_company, phones_numbers, *all_workers):
+        self.name_of_company = name_of_company
+        self.type_of_company = type_of_company
+        self.phones_numbers = phones_numbers
+        self.all_workers = all_workers
 
-    def delete_col(self, col):
-        new_table = list()
-        for elem in self.table:
-            new_table.append([elem[i] for i in range(len(elem)) if i != col])
-        self.table.clear()
-        self.table.extend(new_table)
+    def get_phone(self):
+        if 'contact' in self.phones_numbers:
+            return self.phones_numbers.get('contact')
+        for worker in self.all_workers:
+            if not worker.get_work_phone() is None:
+                return worker.get_work_phone()
 
-    def add_row(self, row):
-        if self.table == [0]:
-            self.table.clear()
-            self.table.append([0])
+    def get_name(self):
+        return self.name_of_company
+
+    def get_sms_text(self):
+        return f'Для компании {self.name_of_company} есть супер предложение! ' \
+            f'Примите участие в нашем беспроигрышном конкурсе для {self.type_of_company}'
+
+
+def send_sms(*args):
+    for user in args:
+        if not user.get_phone() is None:
+            print(f'Отправлено СМС на номер {user.get_phone()} с текстом: {user.get_sms_text()}')
         else:
-            self.table.insert(row, [0 for _ in range(len(self.table[0]) if self.table else 0)])
-
-    def add_col(self, col):
-
-        if len(self.table) > 0:
-            small_ans = list()
-            for i in range(len(self.table)):
-                sp = self.table[i]
-                sp1 = list()
-                flag = False
-                if col == 0:
-                    sp1.append(0)
-                    flag = True
-                for j in range(len(sp)):
-                    if j == col and not flag:
-                        sp1.append(0)
-                        flag = True
-                    sp1.append(sp[j])
-                if not flag:
-                    sp1.append(0)
-                small_ans.append(sp1)
-            self.table.clear()
-            self.table.extend(small_ans)
-        else:
-            self.table.append(0)
+            print(f'Не удалось отправить сообщение абоненту: {user.get_name()}')
