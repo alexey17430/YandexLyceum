142a2097090b87e51bae88e2b04c614da7740caa
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:36:00 +0500

    Таблица

--- Введение в ООП. Полиморфизм.py
+++ Введение в ООП. Полиморфизм.py
@@ -1,19 +1,32 @@
-class MinStat:
-    def __init__(self):
-        self.all_numbers = list()
+class Table:
+    """
+    rows - количество списков внутри списка
+    cols - длина маленьких списков
+    """
+    def __init__(self, rows, cols):
+        self.table = list()
+        for i in range(rows):
+            small_ans = list()
+            for j in range(cols):
+                small_ans.append(0)
+            self.table.append(small_ans)
+        self.rows = rows
+        self.cols = cols
 
-    def add_number(self, chislo):
-        self.all_numbers.append(chislo)
+    def get_value(self, row, col):
+        try:
+            ans = self.table[row][col]
+        except IndexError:
+            ans = None
+        if row < 0 or col < 0:
+            ans = None
+        return ans
 
-    def result(self):
-        return min(self.all_numbers) if self.all_numbers else None
+    def set_value(self, row, col, value):
+        self.table[row][col] = value
 
+    def n_rows(self):
+        return len(self.table)
 
-class MaxStat(MinStat):
-    def result(self):
-        return max(self.all_numbers) if self.all_numbers else None
-
-
-class AverageStat(MinStat):
-    def result(self):
-        return sum(self.all_numbers) / len(self.all_numbers) if self.all_numbers else None
+    def n_cols(self):
+        return len(self.table[0]) if self.table else 0
