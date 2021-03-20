2c89187c23694b85abb5e8b1df5e1d2acbd8783a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-20 16:38:51 +0500

    Точки на плоскости

--- ООП. Определение операторов.py
+++ ООП. Определение операторов.py
@@ -1,17 +1,10 @@
-class Date:
-    def __init__(self, month, day):
-        self.month = month
-        self.day = day
-        self.days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-    def __sub__(self, other):
-        if self.month == other.month and self.day == other.day:
-            return 0
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
 
-        mon1 = self.month
-        mon2 = other.month
-        day1 = self.day
-        day2 = other.day
-        all_days1 = day1 + sum(list(self.days_in_month[i - 1] for i in range(1, mon1)))
-        all_days2 = day2 + sum(list(self.days_in_month[i - 1] for i in range(1, mon2)))
-        return all_days1 - all_days2
+    def __ne__(self, other):
+        return self.x != other.x or self.y != other.y
