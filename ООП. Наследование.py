1c798d7d5694dddf8c8deab97af8cd93f405f0d1
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-24 20:28:34 +0500

    Социальная сеть

--- ООП. Наследование.py
+++ ООП. Наследование.py
@@ -1,22 +1,36 @@
 class User:
-    def solve(self, n):
+    def __init__(self, name):
+        self.name = name
+
+    def send_message(self, user, message):
+        pass
+
+    def post(self, message):
+        pass
+
+    def info(self):
+        return ''
+
+    def describe(self):
         pass
 
 
-class Student(User):
-    pass
+class Person(User):
+    def __init__(self, name, birthday):
+        super().__init__(name)
+        self.birthday = birthday
 
+    def info(self):
+        return f'Дата рождения: {self.birthday}'
 
-class Teacher(User):
-    def check_solution(self, user, n):
+    def subscribe(self, user):
         pass
 
 
-class Admin(User):
-    def edit(self, n):
-        pass
+class Community(User):
+    def __init__(self, name, opisanie):
+        super().__init__(name)
+        self.opisanie = opisanie
 
-
-class SuperAdmin(Admin):
-    def grant(self, user):
-        pass
+    def info(self):
+        return f'Описание: {self.opisanie}'
