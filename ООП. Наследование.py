0cf6f4b33876689719a633d85173c8289563151a
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-24 20:29:22 +0500

    Иерархия транспортных средств

--- ООП. Наследование.py
+++ ООП. Наследование.py
@@ -1,21 +1,110 @@
-class Animal:
-    def breathe(self):
-        pass
-
-    def eat(self):
-        pass
+class Transport:
+    pass
 
 
-class Fish(Animal):
-    def swim(self):
-        pass
+class WaterTransport(Transport):  # Водный транспорт
+    pass
 
 
-class Bird(Animal):
-    def lay_eggs(self):
-        pass
+class Boats(WaterTransport):  # Катера
+    pass
 
 
-class FlyingBird(Bird):
-    def fly(self):
-        pass
+class Steamboats(WaterTransport):  # Пароходы
+    pass
+
+
+class Sailboats(WaterTransport):  # Парусники
+    pass
+
+
+class AirTransport(Transport):  # Воздушный транспорт
+    pass
+
+
+class Aviation(AirTransport):  # Авиация
+    pass
+
+
+class Airplane(Aviation):  # Самолёт
+    pass
+
+
+class ToniStarksCostumes(Aviation):  # Костюмы Тони Старка
+    pass
+
+
+class Aeronautics(AirTransport):  # Воздухоплавание
+    pass
+
+
+class Airships(Aeronautics):  # Дирижабли
+    pass
+
+
+class Balloon(Aeronautics):  # Воздушные шары
+    pass
+
+
+class GroundTransport(Transport):  # Наземный транспорт
+    pass
+
+
+class Railway(GroundTransport):  # Железнодорожный
+    pass
+
+
+class Trains(Railway):  # Поезда
+    pass
+
+
+class Trams(Railway):  # Трамваи
+    pass
+
+
+class Automobile(GroundTransport):  # Автомобильный
+    pass
+
+
+class Tesla(Automobile):  # Тесла
+    pass
+
+
+class Ferrari(Automobile):  # Феррари
+    pass
+
+
+class Lamborgini(Automobile):  # Ламборгини
+    pass
+
+
+class BMW(Automobile):  # БМВ
+    pass
+
+
+class Cycling(GroundTransport):  # Велосипедный
+    pass
+
+
+class DrivenByAnimals(GroundTransport):  # Движимый животными
+    pass
+
+
+class Horse(DrivenByAnimals):  # Конный
+    pass
+
+
+class DogSled(DrivenByAnimals):  # Собачья упряжка
+    pass
+
+
+class SpaceTransport(Transport):  # Космический транспорт
+    pass
+
+
+class SpaceX(SpaceTransport):  # SpaceX
+    pass
+
+
+class RosKosmos(SpaceTransport):  # РосКосмос
+    pass
