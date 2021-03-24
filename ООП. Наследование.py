64d454faa5599b36210e43f269520f5f1737b113
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-03-24 20:29:41 +0500

    Геометрические фигуры – 2

--- ООП. Наследование.py
+++ ООП. Наследование.py
@@ -1,110 +1,34 @@
-class Transport:
+class Shape:
     pass
 
 
-class WaterTransport(Transport):  # Водный транспорт
+class Polygon(Shape):
     pass
 
 
-class Boats(WaterTransport):  # Катера
+class Triangle(Polygon):
     pass
 
 
-class Steamboats(WaterTransport):  # Пароходы
+class IsoscelesTriangle(Triangle):
     pass
 
 
-class Sailboats(WaterTransport):  # Парусники
+class EquilateralTriangle(IsoscelesTriangle):
     pass
 
 
-class AirTransport(Transport):  # Воздушный транспорт
+class Quadrilateral(Polygon):
     pass
 
 
-class Aviation(AirTransport):  # Авиация
+class Parallelogram(Quadrilateral):
     pass
 
 
-class Airplane(Aviation):  # Самолёт
+class Rectangle(Parallelogram):
     pass
 
 
-class ToniStarksCostumes(Aviation):  # Костюмы Тони Старка
-    pass
-
-
-class Aeronautics(AirTransport):  # Воздухоплавание
-    pass
-
-
-class Airships(Aeronautics):  # Дирижабли
-    pass
-
-
-class Balloon(Aeronautics):  # Воздушные шары
-    pass
-
-
-class GroundTransport(Transport):  # Наземный транспорт
-    pass
-
-
-class Railway(GroundTransport):  # Железнодорожный
-    pass
-
-
-class Trains(Railway):  # Поезда
-    pass
-
-
-class Trams(Railway):  # Трамваи
-    pass
-
-
-class Automobile(GroundTransport):  # Автомобильный
-    pass
-
-
-class Tesla(Automobile):  # Тесла
-    pass
-
-
-class Ferrari(Automobile):  # Феррари
-    pass
-
-
-class Lamborgini(Automobile):  # Ламборгини
-    pass
-
-
-class BMW(Automobile):  # БМВ
-    pass
-
-
-class Cycling(GroundTransport):  # Велосипедный
-    pass
-
-
-class DrivenByAnimals(GroundTransport):  # Движимый животными
-    pass
-
-
-class Horse(DrivenByAnimals):  # Конный
-    pass
-
-
-class DogSled(DrivenByAnimals):  # Собачья упряжка
-    pass
-
-
-class SpaceTransport(Transport):  # Космический транспорт
-    pass
-
-
-class SpaceX(SpaceTransport):  # SpaceX
-    pass
-
-
-class RosKosmos(SpaceTransport):  # РосКосмос
+class Square(Rectangle):
     pass
