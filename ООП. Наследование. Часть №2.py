143c303cc3a838775b622b2adc6bb97f5bdab341
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-04-02 13:48:07 +0500

    Треугольники

--- ООП. Наследование. Часть №2.py
+++ ООП. Наследование. Часть №2.py
@@ -1,27 +1,13 @@
-class Profile:
-    def __init__(self, name_of_job):
-        self.name_of_job = name_of_job
+class Triangle:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-    def info(self):
-        return ''
-
-    def describe(self):
-        print(f'{self.name_of_job} {self.info()}')
-
-
-class Vacancy(Profile):
-    def __init__(self, name_of_job, money):
-        super().__init__(name_of_job)
-        self.money = money
-
-    def info(self):
-        return f'Предлагаемая зарплата: {self.money}'
+    def perimeter(self):
+        return self.a + self.b + self.c
 
 
-class Resume(Profile):
-    def __init__(self, name_of_job, age_of_working):
-        super().__init__(name_of_job)
-        self.age_of_working = age_of_working
-
-    def info(self):
-        return f'Стаж работы: {self.age_of_working}'
+class EquilateralTriangle(Triangle):
+    def __init__(self, a):
+        super().__init__(a, a, a)
