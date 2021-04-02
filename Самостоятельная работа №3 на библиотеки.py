12870d77553637ffd5a86874efdd7e15518feba2
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-04-02 17:41:13 +0500

    Урожай фруктов

--- Самостоятельная работа №3 на библиотеки.py
+++ Самостоятельная работа №3 на библиотеки.py
@@ -1,17 +1,11 @@
-import datetime as dt
+from PIL import Image, ImageDraw
 
 
-def before_the_concert(first, second):
-
-    first = list(map(int, first.split('-')))
-    second = list(map(int, second.split('-')))
-    first = dt.date(first[0], first[1], first[2])
-    second = dt.date(second[0], second[1], second[2])
-    now = dt.datetime.now().date()
-
-    if int(first.strftime("%w")) == 1 or int(first.strftime("%w")) == 3:
-        first += dt.timedelta(days=1)
-    if int(second.strftime("%w")) == 1 or int(second.strftime("%w")) == 3:
-        second += dt.timedelta(days=1)
-
-    return int((first - now).days), int((second - now).days)
+def fruits(size, bg_color, *houses):
+    im = Image.new('RGB', size, bg_color)
+    #  width - ширина, height - высота
+    draw = ImageDraw.Draw(im)
+    for elem in houses:
+        x, y, r = elem
+        draw.ellipse((x - r, y - r, x + r, y + r), fill=((r * 123) % 256, (r * 123) % 256, (r * 123) % 256))
+    return im
