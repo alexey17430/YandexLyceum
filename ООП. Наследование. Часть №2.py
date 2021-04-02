dcc1d3b09ad212d7e6f70e0079d564b0e8973162
Rybolovlev Alexey <rybolovlevalexey@yandex.ru>
2021-04-02 13:48:25 +0500

    Сумматоры – 2

--- ООП. Наследование. Часть №2.py
+++ ООП. Наследование. Часть №2.py
@@ -1,13 +1,24 @@
-class Triangle:
-    def __init__(self, a, b, c):
-        self.a = a
+class Summator:
+    def transform(self, n):
+        return list(range(1, n + 1))
+
+    def sum(self, n):
+        return sum(self.transform(n))
+
+
+class PowerSummator(Summator):
+    def __init__(self, b):
         self.b = b
-        self.c = c
 
-    def perimeter(self):
-        return self.a + self.b + self.c
+    def transform(self, n):
+        return list(i ** self.b for i in range(1, n + 1))
 
 
-class EquilateralTriangle(Triangle):
-    def __init__(self, a):
-        super().__init__(a, a, a)
+class SquareSummator(PowerSummator):
+    def __init__(self):
+        super().__init__(2)
+
+
+class CubeSummator(PowerSummator):
+    def __init__(self):
+        super().__init__(3)
